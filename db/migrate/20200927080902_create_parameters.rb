3162867fbb8533158b881e110191664a123a85e1
Mizuki Zenta <mizukisb@gmail.com>
2020-09-29 21:31:26 +1000

    Add apptitude index

--- db/migrate/20200927080902_create_parameters.rb
+++ db/migrate/20200927080902_create_parameters.rb
@@ -3,16 +3,22 @@ class CreateParameters < ActiveRecord::Migration[6.0]
     create_table :parameters do |t|
       t.integer :life
       t.integer :life_apptitude
+      t.integer :life_apptitude_index
       t.integer :power
       t.integer :power_apptitude
+      t.integer :power_apptitude_index
       t.integer :intelligence
       t.integer :intelligence_apptitude
+      t.integer :intelligence_apptitude_index
       t.integer :accuracy
       t.integer :accuracy_apptitude
+      t.integer :accuracy_apptitude_index
       t.integer :avoidance
       t.integer :avoidance_apptitude
+      t.integer :avoidance_apptitude_index
       t.integer :defence
       t.integer :defence_apptitude
+      t.integer :defence_apptitude_index
       t.references :monster
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -59,16 +59,22 @@ ActiveRecord::Schema.define(version: 2020_09_27_102212) do
   create_table "parameters", force: :cascade do |t|
     t.integer "life"
     t.integer "life_apptitude"
+    t.integer "life_apptitude_index"
     t.integer "power"
     t.integer "power_apptitude"
+    t.integer "power_apptitude_index"
     t.integer "intelligence"
     t.integer "intelligence_apptitude"
+    t.integer "intelligence_apptitude_index"
     t.integer "accuracy"
     t.integer "accuracy_apptitude"
+    t.integer "accuracy_apptitude_index"
     t.integer "avoidance"
     t.integer "avoidance_apptitude"
+    t.integer "avoidance_apptitude_index"
     t.integer "defence"
     t.integer "defence_apptitude"
+    t.integer "defence_apptitude_index"
     t.bigint "monster_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
