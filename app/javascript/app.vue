c38e5e46caa551fcf839301314407e94c85e2460
Mizuki Zenta <mizukisb@gmail.com>
2020-10-01 20:50:45 +1000

    Add parameter chart component

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,19 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+  <v-app>
+    <!-- <ApptitudeChart :chartData="[4,5,5,4,2,3]" /> dynamically add the chart data :) -->
+  </v-app>
 </template>
 
 <script>
+import ApptitudeChart from './components/ApptitudeChart'
 export default {
+  components:{
+    ApptitudeChart,
+  },
   data: function () {
-    return {
-      message: "Hello Vue!"
-    }
+    return {}
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
-</style>
+<style scoped></style>
---
+++ app/javascript/components/ApptitudeChart.vue
@@ -0,0 +1,38 @@
+<script>
+import { Radar } from 'vue-chartjs'
+export default {
+  name: 'ApptitudeChart',
+  extends: Radar,
+  props: ['chartData'],
+  mounted () {
+    this.renderChart({
+      labels: ['ライフ', 'ちから', 'かしこさ', '命中', '回避', '丈夫さ'],
+      datasets: [ {
+        data: this.chartData,
+        backgroundColor: 'rgba(95, 248, 218, 0.4)',
+        borderColor: 'rgba(35, 151, 128 , 0.7)'
+        } ] },
+      
+    {
+      title: {display: true, text: 'パラメーター適性', fontSize: 16},
+      legend: { display: false },
+      tooltips: { enabled: false },
+      responsive: true,
+      maintainAspectRatio: false,
+      scale: {
+        pointLabels :{ fontStyle: "bold", fontSize: 12 },
+        ticks: {
+          fontStyle: 'bold',
+          beginAtZero: true,
+          stepSize: 1,
+          max: 5,
+          userCallback: function (value, index, values) {
+            // converting numbes to ranks
+            return ['E','D','C','B','A'][value - 1]
+          }
+        }
+      }
+    })
+  }
+}
+</script>
