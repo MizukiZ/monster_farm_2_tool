cd01c06d2135896c89931ac66221d3aac8dcdc9e
Mizuki Zenta <mizukisb@gmail.com>
2020-10-01 22:08:54 +1000

    Refactor app.vue

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,30 @@
 <template>
   <v-app>
-    <ApptitudeChart :apptitudeData="[4,5,5,4,2,3]" />
-    <ParameterChart :parameterData="[120,170,190,100,80,130]"/>
+    <v-container>
+      <v-row>
+        <v-col sm="6">
+          <ParameterChart :parameterData="[120, 170, 190, 100, 80, 130]" />
+        </v-col>
+        <v-col sm="6">
+          <ApptitudeChart :apptitudeData="[4, 5, 5, 4, 2, 3]" />
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import ApptitudeChart from './components/ApptitudeChart'
-import ParameterChart from './components/ParameterChart'
+import ApptitudeChart from "./components/ApptitudeChart";
+import ParameterChart from "./components/ParameterChart";
 export default {
-  components:{
+  components: {
     ApptitudeChart,
     ParameterChart,
   },
   data: function () {
-    return {}
-  }
-}
+    return {};
+  },
+};
 </script>
 
 <style scoped></style>
