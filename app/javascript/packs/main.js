018aae9c12ef3afc7ab7d7aaabf6edbb8bfba60d
Mizuki Zenta <mizukisb@gmail.com>
2020-10-13 20:13:17 +1100

    Add plugin

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import vuetify from '../plugins/vueitfy'
 import App from '../app.vue'
-
-Vue.use(Vuetify)
 
 // Global variables
 Vue.prototype.$parameterWordsArray = ["ライフ", "ちから", "かしこさ", "命中", "回避", "丈夫さ"]
@@ -28,10 +25,8 @@ Vue.prototype.$colors = {
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
-    vuetify: new Vuetify(),
+    vuetify,
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
-
-  console.log(app)
 })
