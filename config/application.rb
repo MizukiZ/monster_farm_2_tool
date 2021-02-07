078021952f3d700fda2582910b7eb1907a368248
Mizuki Zenta <mizukisb@gmail.com>
2021-02-07 21:07:53 +1100

    Change default loader to 6.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module MonsterFarm2App
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
     config.eager_load_paths << Rails.root.join("lib")
     # Configuration for the application, engines, and railties goes here.
     #
